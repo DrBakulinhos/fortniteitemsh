0673ef2ba24cdeb28e27a4011dd240514aa66852
DrBakulinhos <90698076+DrBakulinhos@users.noreply.github.com>
2021-09-14 14:46:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-#Please do not change anything to this section!
+
 import sys
 import os
 import time
@@ -14,7 +14,7 @@ OAUTH_TOKEN = os.environ['TWITTER_OAUTH_TOKEN']
 OAUTH_TOKEN_SECRET = os.environ['TWITTER_OAUTH_TOKEN_SECRET']
 TWEET_LENGTH = 280
 TWEET_URL_LENGTH = 21
-#Please so not change anything to this section!
+
 
 today = datetime.now()
 tz = pytz.timezone('Asia/Manila') # change the timezone to your desired/home timezone.
@@ -25,8 +25,8 @@ def twitter_handle():
 
 photo = open('./output/shop.png', 'rb')
 
-#Change the text into anything you like! (and please change the variables accordingly if you changed it above.)
-tweetStr = 'Fortnite item shop for '+pht.strftime("%m/%d/%y")+'!\n'+pht.strftime("%I:%M %p")+', PHT/GMT+8\n\nIf you want to support me, make sure to use code \"KuletXCore\" on the Fortnite Item Shop!\nReally appreciate it!'
+
+tweetStr = 'Fortnite item shop for '+pht.strftime("%m/%d/%y")+'!\n'+pht.strftime("%I:%M %p")+', PHT/GMT+8\n\nIf you want to support me, make sure to use code \"GK\" on the Fortnite Item Shop!\nReally appreciate it!'
 
 api = twitter_handle()
 response = api.upload_media(media=photo)
